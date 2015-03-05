90948a23f78fc3f27c7e10e96e26999e776011a0
tdos.apone <toemossgarcia@gmail.com>
2015-03-05 17:58:31 -0600

    Added movement+color threshold demo.

--- framebuffer.py
+++ framebuffer.py
@@ -1,49 +1,92 @@
 import cv2
 import numpy as np
+from abc import ABCMeta, abstractmethod
 VERBOSE = 0
 
-class FrameBuffer(object):
-    """
-    Class for handling video files/streams generically through opencv's video captures class
-    """
-    def __init__(self,src,stop=None,start=None):
-        if start is not None: start, stop = stop, start
-        self.cap = cv2.VideoCapture(src)
-        self.is_video = isinstance(src,basestring)
-        self.name = str(src)
-        
-        h = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.dim = (h,w)
 
+class FrameBufferBase(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def read(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def close(self):
+        raise NotImplementedError
+
+class VideoBuffer(FrameBufferBase):
+    def __init__(self,src,start=None,stop=None):
         self.start = start
         self.stop = stop
-        if self.is_video:
+
+        self.__buff = cv2.VideoCapture(src)
+
+        if isinstance(src,basestring):
             if self.start is None:
-                self.start = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+                self.start = int(self.__buff.get(cv2.CAP_PROP_POS_FRAMES))
             if self.stop is None:
-                self.stop = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT)+self.cap.get(cv2.CAP_PROP_POS_FRAMES))
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.start)
+                self.stop = int(self.__buff.get(cv2.CAP_PROP_FRAME_COUNT)+self.__buff.get(cv2.CAP_PROP_POS_FRAMES))
+            self.__buff.set(cv2.CAP_PROP_POS_FRAMES, self.start)
         else:
             self.start = 0
             self.stop = -1
-        self.idx = self.start-1
 
-        if VERBOSE:
-            print "Opened %r (name='%s',range=(%s,%s),dim=%s)" % (self.cap,self.name,self.start,self.stop,self.dim)
+        self._idx = self.start-1
 
     def read(self):
-        self.idx += 1
-        if self.is_video and self.idx == self.stop:
-            return False, np.array([])
+        self._idx += 1
+        if self._idx == self.stop:
+            return np.array([])
 
-        return self.cap.read()
+        valid, img = self.__buff.read()
+        if valid:
+            return img
+        return np.array([])
 
     def close(self):
-        self.cap.release()
+        self.__buff.release()
+
+
+class ImageBuffer(FrameBufferBase):
+    def __init__(self,src,start=None,stop=None):
+        self.start = start
+        self.stop = stop
+
+        self.__buff = (cv2.imread(fn,**kwargs) for fn in list(src)[start:stop])
+
+    def read(self):
+        try:
+            return self.__buff.next()
+        except StopIteration:
+            return np.array([])
+
+    def close(self):
+        None
+
+        
+class FrameBuffer(object):
+    def __init__(self,src,stop=None,start=None):
+        """
+        Class for handling video files/streams generically through opencv's video captures class
+        """
+        if start is not None: start, stop = stop, start
+
+        if hasattr(src,'__iter__'):
+            buff = ImageBuffer
+        else:
+            buff = VideoBuffer
+        self.__cap = buff(src,start,stop)
+
+        if VERBOSE:
+            print "Opened %r (range=(%s,%s))" % (self.__cap,self.start,self.stop)
+        
+    def __getattr__(self,attr):
+        return getattr(self.__cap, attr)
 
     def __enter__(self):
         return self
 
     def __exit__(self):
         self.close()
+
---
+++ threshold.py
@@ -0,0 +1,103 @@
+import sys,os
+sys.path.insert(0, os.path.abspath('../'))
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import framebuffer as fb
+
+
+alpha = 0.5
+T0 = 20
+
+fig = plt.figure()
+axes = {}
+axes['raw'] = plt.subplot2grid((3,2), (0, 0), colspan=2)
+axes['bkgnd'] = plt.subplot2grid((3,2), (1, 0))
+axes['thresh'] = plt.subplot2grid((3,2), (1, 1))
+axes['moving'] = plt.subplot2grid((3,2), (2, 0))
+axes['moving&skin'] = plt.subplot2grid((3,2), (2, 1))
+
+for k,ax in axes.items():
+    ax.set_title(k)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+get_imdisp = lambda ax: ax.findobj(mpl.image.AxesImage)[0]
+
+fb.VERBOSE = 1
+
+
+cap = fb.FrameBuffer(sys.argv[1] if len(sys.argv)>1 else -1, *map(int,sys.argv[2:]))
+try:
+    prev = cap.read()
+    curr = cap.read()
+
+    prevg = cv2.cvtColor(prev,cv2.COLOR_BGR2GRAY)
+    currg = cv2.cvtColor(curr,cv2.COLOR_BGR2GRAY)
+    T = np.ones_like(currg)*T0
+    bkgnd = currg.copy()
+
+    axes['raw'].imshow(curr)
+    axes['bkgnd'].imshow(bkgnd,cmap=mpl.cm.get_cmap('gray'))
+    axes['thresh'].imshow(bkgnd,cmap=mpl.cm.get_cmap('gray'))
+    axes['moving'].imshow(bkgnd,cmap=mpl.cm.get_cmap('gray'))
+    axes['moving&skin'].imshow(bkgnd,cmap=mpl.cm.get_cmap('gray'))
+    fig.tight_layout()
+
+    next = cap.read()
+    krn = np.ones((3,3),dtype=np.uint8)
+    rownums = np.arange(currg.shape[0],dtype=int).reshape(-1,1)
+    colnums = np.arange(currg.shape[1],dtype=int).reshape(1,-1)
+    while plt.pause(1e-6) is None and next.size:
+        nextg = cv2.cvtColor(next,cv2.COLOR_BGR2GRAY)
+
+        moving = (cv2.absdiff(prevg,nextg) > T) & (cv2.absdiff(currg,nextg) > T)
+        cv2.medianBlur(moving.view(np.uint8),3,dst=moving.view(np.uint8))
+
+        # estimate centroid and bounding box
+        area = np.sum(moving)
+        dispimg = next.copy()
+        mask = moving.copy()
+        if area > 100:
+            mov_cols = mask*colnums
+            mov_rows = mask*rownums
+            x = np.sum(mov_cols) / area
+            y = np.sum(mov_rows) / area
+            x0,x1 = np.min(mov_cols[mask]), np.max(mov_cols[mask])
+            y0,y1 = np.min(mov_rows[mask]), np.max(mov_rows[mask])
+
+            cv2.circle(dispimg,(x,y),5,color=(0,255,0),thickness=-1)
+            cv2.rectangle(dispimg,(x0,y0),(x1,y1),color=(0,204,255),thickness=2)
+
+            cimg = cv2.cvtColor(curr[y0:y1+1,x0:x1+1,:],cv2.COLOR_BGR2YCR_CB)
+            cr,cb = cimg[:,:,1], cimg[:,:,2]
+            skinmask = (77 <= cb)&(cb <= 127)
+            skinmask &= (133 <= cr)&(cr <= 173)
+            mask[y0:y1+1,x0:x1+1] = skinmask
+
+        # draw
+        get_imdisp(axes['raw']).set_data(dispimg[:,:,::-1])
+        get_imdisp(axes['bkgnd']).set_data(bkgnd)
+        get_imdisp(axes['thresh']).set_data(T)
+        get_imdisp(axes['moving']).set_data(moving*255)
+        get_imdisp(axes['moving&skin']).set_data(mask*255)
+        for ax in axes.values(): fig.canvas.blit(ax.bbox)
+
+        # Updating threshold depends on current background model
+        # so always update this before updating background
+        T[~moving] = alpha*T[~moving] + (1-alpha)*5*cv2.absdiff(nextg,bkgnd)[~moving]
+        T[T<T0] = T0
+        # T[moving] = T[moving]
+        bkgnd[~moving] = alpha*bkgnd[~moving] + (1-alpha)*nextg[~moving]
+        bkgnd[moving] = nextg[moving]
+
+        prev, prevg = curr, currg
+        curr, currg = next, nextg
+        next = cap.read()
+except KeyboardInterrupt:
+    pass
+finally:
+    plt.close(fig)
+    cap.close()
