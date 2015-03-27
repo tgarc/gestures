12e5364502c8385548961de527dea2e3a0e5de8c
tdos.apone <toemossgarcia@gmail.com>
2015-03-26 20:13:04 -0500

    tracking_demo: added state machine&refactoring&threading
    + Figure blitting is now being done in a timed thread

--- common.py
+++ common.py
@@ -1,4 +1,26 @@
+import numpy as np
+
+
 trunc_coords = lambda shape,xy: tuple(x if x >= 0 and x <= dimsz
                                       else (0 if x < 0 else dimsz)
                                       for dimsz,x in zip(shape[::-1],xy))
 inttuple = lambda xy: tuple(map(int,xy))
+
+def findBBoxCoM(mask,roi=None):
+    rownums = np.arange(mask.shape[0],dtype=int)
+    colnums = np.arange(mask.shape[1],dtype=int)
+
+    if roi:
+        x0,y0,x1,y1 = roi
+        mask = mask[y0:y1,x0:x1]
+    else:
+        x0,y0,x1,y1 = 0,0,mask.shape[1],mask.shape[0]
+
+    masked_cols = mask*colnums[x0:x1].reshape(1,-1)
+    masked_rows = mask*rownums[y0:y1].reshape(-1,1)
+    x0,x1 = np.min(masked_cols[mask]), np.max(masked_cols[mask])+1
+    y0,y1 = np.min(masked_rows[mask]), np.max(masked_rows[mask])+1
+    xcom = np.sum(masked_cols)//np.sum(mask)
+    ycom = np.sum(masked_rows)//np.sum(mask)
+
+    return (x0,y0,x1,y1),(xcom,ycom)
--- tracking_demo.py
+++ tracking_demo.py
@@ -7,11 +7,15 @@ import matplotlib as mpl
 import framebuffer as fb
 from gesture_classifier import dollar
 import time
+from itertools import cycle
 from common import *
+import threading
 
 
 mpl.use("TkAgg")
 
+STATELEN = 5
+STATECYCLE = cycle(('wait','search','track'))
 
 alpha = 0.5
 T0 = 30
@@ -46,8 +50,8 @@ try:
     waypts = []
     MAXLEN = min(imshape)//2
     blobthresh_hi = (min(imshape)//4)**2
-    blobthresh_lo = blobthresh_hi//2
-    T_move = 100#np.pi*min(imshape)//2
+    blobthresh_lo = 3*blobthresh_hi//4
+    T_move = 150#np.pi*min(imshape)//2
 
     T = np.ones_like(imgq_g[0])*T0
     bkgnd = imgq_g[0].copy()
@@ -57,7 +61,6 @@ try:
     axes['draw'].set_xlim(0,imshape[1])
 
     fig.tight_layout()
-    fig.show()
     bg_cache = {ax:fig.canvas.copy_from_bbox(ax.bbox) for ax in axes.values()}
 
     draw_state = 0
@@ -75,19 +78,44 @@ try:
         fig.canvas.draw()
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
 
-    tracking=False
-    t_loop=time.time()
-    rownums = np.arange(imshape[0],dtype=int)
-    colnums = np.arange(imshape[1],dtype=int)
+    timer = fig.canvas.new_timer(interval=50)
+    figlock= threading.Lock()
+    def update():
+        figlock.acquire() # wait for data to be ready
+        for ax in fig.axes: fig.canvas.blit(ax.bbox)
+        figlock.release()
+    timer.add_callback(update)
+    timer.start()
+    fig.show()
+
+    state=STATECYCLE.next()
+    statecnt = 0
+    def CountState():
+        global state, statecnt
+        statecnt += 1
+        if statecnt == STATELEN:
+            statecnt = 0
+            state = STATECYCLE.next()
+
     krn = np.ones((3,3),dtype=np.uint8)
     bkproject = np.zeros_like(bkgnd)
     while imgq[-1].size:
         imgq_g[-1] = cv2.cvtColor(imgq[-1],cv2.COLOR_BGR2GRAY)
         dispimg = imgq[-1].copy()
-        
-        moving = (cv2.absdiff(imgq_g[0],imgq_g[-1]) > T) & (cv2.absdiff(imgq_g[1],imgq_g[-1]) > T)
-        # cv2.medianBlur(moving.view(np.uint8),3,dst=moving.view(np.uint8))
 
+        # motion detection
+        moving = (cv2.absdiff(imgq_g[0],imgq_g[-1]) > T) & (cv2.absdiff(imgq_g[1],imgq_g[-1]) > T)
+        movesum = movearea = np.sum(moving)
+        if movearea:
+            movesum = movearea
+            move_roi = findBBoxCoM(moving)[0]
+            x0,y0,x1,y1 = move_roi
+            movearea = (x1-x0)*(y1-y0)
+
+            print "Moving:", (x0+x1)//2, (y0+y1)//2, x1-x0, y1-y0
+            cv2.rectangle(dispimg,(x0,y0),(x1,y1),color=(0,255,0),thickness=2)
+
+        # skin segementation
         img_crcb = cv2.cvtColor(imgq[-1],cv2.COLOR_BGR2YCR_CB)
         cr,cb = img_crcb[:,:,1], img_crcb[:,:,2]
         skin = (77 <= cb)&(cb <= 127)
@@ -109,89 +137,49 @@ try:
         elif draw_state == 3:
             dispimg = cv2.cvtColor(bkproject*255,cv2.COLOR_GRAY2BGR)
             
-        movesum = movearea = np.sum(moving)
-        if movearea:
-            # Calculate bbox of moving pixels
-            movesum = movearea
-            mov_cols = moving*colnums.reshape(1,-1)
-            mov_rows = moving*rownums.reshape(-1,1)
-            x0,x1 = np.min(mov_cols[moving]), np.max(mov_cols[moving])+1
-            y0,y1 = np.min(mov_rows[moving]), np.max(mov_rows[moving])+1
-            movearea = (x1-x0)*(y1-y0)
+        if state == 'wait':
+            if statecnt == 0:
+                if len(waypts) > 10:
+                    # Find best gesture match
+                    x,y = zip(*waypts)
+                    matches = dollar.query(x,y,250,64,templates_fh)
+                    score,theta,clsid = matches[0]
 
-        if tracking and movearea > blobthresh_lo and movesum > T_move:
-            # Actively tracking gesture
-            cv2.rectangle(dispimg,(x0,y0),(x1,y1),color=(0,255,0),thickness=2)
+                    ds = templates_fh[clsid][0]
+                    x,y = dollar.preprocess(x,y,250,64)
 
-            bkproject = cv2.calcBackProject([img_crcb],chans,hist,ranges,1)
-            movereg = np.zeros_like(moving)
-            movereg[y0:y1,x0:x1] = True
-            bkproject &= movereg
+                    # clean up last match
+                    figlock.acquire()
+                    del axes['match'].lines[:]
+                    fig.canvas.restore_region(bg_cache[axes['match']])
 
-            # notice we're using the track_bbox from last iteration
-            # for the intitial estimate
-            niter, track_bbox = cv2.meanShift(bkproject,track_bbox,term_crit)
-            x,y,w,h = track_bbox
-            x0,y0,x1,y1 = x,y,x+w,y+h
+                    # Show preprocessed gesture and closest matching template
+                    axes['match'].add_line(plt.Line2D(ds['x'],ds['y'],marker='x',color='g'))
+                    axes['match'].add_line(plt.Line2D(x,y,marker='o',color='b'))
 
-            skin_roi = skin[y0:y1,x0:x1]
-            skin_cols = skin_roi*colnums[x0:x1].reshape(1,-1)
-            skin_rows = skin_roi*rownums[y0:y1].reshape(-1,1)
-            xcom = np.sum(skin_cols)//np.sum(skin_roi)
-            ycom = np.sum(skin_rows)//np.sum(skin_roi)
+                    for l in axes['match'].lines: axes['match'].draw_artist(l)
 
-            cv2.rectangle(dispimg,(x0,y0),(x1,y1),color=(0,204,255),thickness=2)
-            cv2.circle(dispimg,(xcom,ycom),5,(0,255,0),thickness=-1)
-            waypts.append((xcom,ycom))
+                    axes['match'].set_title("%s (score: %.2f)" % (clsid,score))
 
-            print "Skin Tracking:",x0,y0,x1,y1
-        elif tracking:
-            # End of gesture
-            tracking = False
-            if len(waypts) > 10:
-                # Find best gesture match
-                x,y = zip(*waypts)
-                matches = dollar.query(x,y,250,64,templates_fh)
-                score,theta,clsid = matches[0]
+                    fig.canvas.draw() # only way I know how to update text regions
+                    figlock.release()
 
-                ds = templates_fh[clsid][0]
-                x,y = dollar.preprocess(x,y,250,64)
-
-                # clean up last match
-                del axes['match'].lines[:]
-                fig.canvas.restore_region(bg_cache[axes['match']])
-
-                # Show preprocessed gesture and closest matching template
-                axes['match'].add_line(plt.Line2D(ds['x'],ds['y'],marker='x',color='g'))
-                axes['match'].add_line(plt.Line2D(x,y,marker='o',color='b'))
-                for l in axes['match'].lines: axes['match'].draw_artist(l)
-                axes['match'].set_title("%s (score: %.2f)" % (clsid,score))
-                fig.canvas.draw() # only way I know how to update text regions
-
-                print "Class: %s (%.2f)" % (clsid,score)
-                print "Npoints:", len(waypts)
-            # remove this gesture from the drawing board
-            del axes['draw'].lines[:]
-            waypts = []
-        elif movearea > blobthresh_hi:
-            # Gesture candidate detected
-            cv2.rectangle(dispimg,(x0,y0),(x1,y1),color=(0,255,0),thickness=2)
-            print "Moving:", (x0+x1)//2, (y0+y1)//2, x1-x0, y1-y0
-            
-            crcb_roi = img_crcb[y0:y1,x0:x1]
-            skin_roi = skin[y0:y1,x0:x1]
-            if np.sum(skin_roi) > (crcb_roi.size//12):
-                # Gesture candidate accepted
-                tracking = True
+                    print "Class: %s (%.2f)" % (clsid,score)
+                    print "Npoints:", len(waypts)
+                # remove this gesture from the drawing board
+                waypts = []
+            CountState()
+        elif state == 'track':
+            x0,y0,x1,y1 = move_roi
+            if statecnt == 0:
+                CountState() # Make initial hand centroid estimate
 
                 # Estimate hand centroid as the centroid of skin colored pixels
                 # inside the bbox of detected movement
-                skin_cols = skin_roi*colnums[x0:x1].reshape(1,-1)
-                skin_rows = skin_roi*rownums[y0:y1].reshape(-1,1)
-                xcom = np.sum(skin_cols)//np.sum(skin_roi)
-                ycom = np.sum(skin_rows)//np.sum(skin_roi)
-                x0,x1 = np.min(skin_cols[skin_roi]), np.max(skin_cols[skin_roi])+1
-                y0,y1 = np.min(skin_rows[skin_roi]), np.max(skin_rows[skin_roi])+1
+                crcb_roi = img_crcb[y0:y1,x0:x1]
+                skin_roi = skin[y0:y1,x0:x1]
+                (x0,y0,x1,y1),(xcom,ycom) = findBBoxCoM(skin,(x0,y0,x1,y1))
+                waypts.append((xcom,ycom))
 
                 # Use the hand centroid estimate as our initial estimate for
                 # tracking
@@ -202,8 +190,7 @@ try:
                 h = min(2*min((y1-ycom,ycom-y0)),MAXLEN)
                 w = min(x1-x0,MAXLEN)
                 track_bbox = xcom-w//2,ycom-h//2,w,h                
-                waypts.append((xcom,ycom))
-                
+
                 # Use the skin bbox/centroid to initiate tracking
                 hist = cv2.calcHist([crcb_roi], chans, skin_roi.view(np.uint8), nbins, ranges)
                 # Normalize to 1 to get the sample PDF
@@ -214,7 +201,47 @@ try:
                 cv2.circle(dispimg,(xcom,ycom),5,(0,255,0),thickness=-1)
 
                 # Add this gesture to the drawing board
+                figlock.acquire()
+                del axes['draw'].lines[:]
+                fig.canvas.restore_region(bg_cache[axes['draw']])
                 axes['draw'].add_line(plt.Line2D((),(),marker='o',color='b'))
+                fig.canvas.draw()
+                figlock.release()
+            elif movearea > blobthresh_lo and movesum > T_move:
+                bkproject = cv2.calcBackProject([img_crcb],chans,hist,ranges,1)
+                movereg = np.zeros_like(moving)
+                movereg[y0:y1,x0:x1] = True
+                bkproject &= movereg
+
+                # notice we're using the track_bbox from last iteration
+                # for the intitial estimate
+                niter, track_bbox = cv2.meanShift(bkproject,track_bbox,term_crit)
+                x,y,w,h = track_bbox
+                x0,y0,x1,y1 = x,y,x+w,y+h
+
+                # xcom,ycom = findBBoxCoM(skin,(x0,y0,x1,y1))[1]
+                xcom = (x0+x1)/2
+                ycom = (y0+y1)/2
+
+                cv2.rectangle(dispimg,(x0,y0),(x1,y1),color=(0,204,255),thickness=2)
+                cv2.circle(dispimg,(xcom,ycom),5,(0,255,0),thickness=-1)
+                waypts.append((xcom,ycom))
+
+                print "Skin Tracking:",x0,y0,x1,y1
+            else:
+                CountState() # Tracking failed this frame
+        elif state == 'search':
+            if movearea > blobthresh_hi:
+                # Gesture candidate detected
+                x0,y0,x1,y1 = move_roi
+
+                # check that proportion of skin to are of movement bbox is high
+                # enough
+                if np.sum(skin[y0:y1,x0:x1]) > ((y1-y0)*(x1-x0)//16):
+                    CountState()    # Increase trust for gesture candidate
+
+        # wait for drawing to finish
+        figlock.acquire()
 
         # Draw image
         get_imdisp(axes['raw']).set_data(dispimg[:,:,::-1])
@@ -226,9 +253,8 @@ try:
             axes['draw'].lines[0].set_data(zip(*waypts))
             axes['draw'].draw_artist(axes['draw'].lines[0])
 
-        # Update figure
-        for ax in axes.values():
-            fig.canvas.blit(ax.bbox)
+        # Finished updating data
+        figlock.release()
 
         # Updating threshold depends on current background model
         # so always update this before updating background
@@ -242,13 +268,7 @@ try:
         # shift buffer left        
         imgq[:-1] = imgq[1:] 
         imgq_g[:-1] = imgq_g[1:]
-
-        t = time.time()-t_loop
-        while t < 0.0667: t = time.time()-t_loop
-        # print round(1/t,2)
-
         imgq[-1] = cap.read()
-        t_loop = time.time()
         fig.canvas.get_tk_widget().update()
 
 except KeyboardInterrupt:
