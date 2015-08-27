e685d31f07bbe73eeb95ada34b904614062906b3
tdos.apone <toemossgarcia@gmail.com>
2015-08-27 16:37:42 -0500

    works like wanted aside from an indexerror

--- gestures/core/gui.py
+++ gestures/core/gui.py
@@ -2,32 +2,42 @@ from matplotlib.animation import Animation
 import matplotlib.pyplot as plt
 import itertools
 import warnings
+from collections import deque
 
-
-class GUI(Animation):
-    def __init__(self,fig,init_func=None,interval=1):
-        self.__data = []
+class AsyncAnimation(Animation):
+    def __init__(self,fig,init_artists=[],event_source=None,interval=1):
+        self.__data = deque()
         self.__lag = 0
 
         self._fig = fig
-        self._framedata = itertools.count()
-        self._init_func = init_func
         self._interval = interval
-        self._drawn_artists = []
+        self._init_artists = init_artists
 
-        timer = fig.canvas.new_timer(interval=self._interval)
-        self.timer = timer
+        if event_source is None:
+            event_source = fig.canvas.new_timer(interval=self._interval)
 
-        Animation.__init__(self,fig,event_source=timer,blit=True)
-        # self._fig.canvas.draw()
-        # self._fig.canvas.flush_events()
-        # self._fig.canvas.start_event_loop(1e-6)
-        # self._fig.canvas.start_event_loop(1e-6)
-        plt.pause(1e-6)
+        if callable(self._init_artists):
+            artists = self._init_artists()
+        else:
+            artists = self._init_artists
+        self.__data.append((zip(artists,[None]*len(artists)),False))
+
+        Animation.__init__(self,fig,event_source=event_source,blit=True)
+
+    def new_frame_seq(self):
+        return itertools.count()
+
+    def _init_draw(self):
+        # Initialize the drawing either using the given init_func or by
+        # calling the draw function with the first item of the frame sequence.
+        # For blitting, the init_func should return a sequence of modified
+        # artists.
+        # self._drawn_artists = self._init_artists()
+        # self.__data.append((zip(self._drawn_artists,[None]*len(self._drawn_artists)),False))
+        self._draw_frame(next(self.new_frame_seq()))
 
     def update_artists(self,artists,redraw=False):
-        if self.event_source is None:
-            raise Exception
+        assert(self.event_source is not None)
 
         if len(self.__data) == self.__lag > 1: 
             warnings.warn("Artists queue is behind by %d" % len(self.__data))
@@ -37,15 +47,17 @@ class GUI(Animation):
         self._fig.canvas.flush_events()
 
     def _draw_next_frame(self, *args):
+        # carry on if there's nothing to draw right now
         if self.__data:
             Animation._draw_next_frame(self,*args)
 
     def _draw_frame(self,framedata):
-        artdata, redraw = self.__data.pop(0)
+        artdata, redraw = self.__data.popleft()
 
         artists = []
         for (a,d) in artdata:
             artists.append(a)
-            a.set_data(d)
+            if d is not None:
+                a.set_data(d)
 
         self._drawn_artists = artists
